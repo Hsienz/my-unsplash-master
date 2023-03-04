e8fc2fd61ec645ec686708d536fcb8c8a202c78b
Hsienz <55347238+Hsienz@users.noreply.github.com>
2023-03-04 09:38:27 +0800

    no refresh submit

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,9 +1,8 @@
 import { useJumpUp } from "@/contexts/JumpUpContext";
 import Input from "./Input";
-import React from "react";
+import React, { FormEvent } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
-import Image from "next/image";
 
 const Upload = () => {
 	const { setComponent } = useJumpUp();
@@ -11,11 +10,47 @@ const Upload = () => {
 	const handleCancel = () => {
 		setComponent(null);
 	};
+	const checkImage = (url: string) => {
+		const img = new Image();
+		img.src = url;
+		return new Promise(
+			(resolve: (res: string) => void, reject: (err: string) => void) => {
+				img.onload = () => resolve("");
+				img.onerror = () => reject("Invalid URL");
+			}
+		);
+	};
+	const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+		e.preventDefault();
+		let data:{[key:string]:string} = {
+
+		}
+		for (let i = 0; i < e.currentTarget.length; i++) {
+			const el = e.currentTarget[i] as HTMLInputElement;
+			data[el.name] = el.value;
+		}
+		delete data[""]
+		let url = data["photoUrl"];
+		const body = JSON.stringify(data)
+		console.log(body)
+		checkImage(url)
+			.then(async () => {
+				await fetch("/api/addPhoto", {
+					method: "POST",
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body,
+				});
+			})
+			.catch((err) => {
+				alert(err);
+			});
+	};
 	return (
 		<form
-			method="POST"
-			action="/api/addPhoto"
 			className="jump flex flex-col w-[640px] gap-4 justify-between"
+			onSubmit={handleSubmit}
 		>
 			<h2>Add a new photo</h2>
 			<input type="hidden" name="uid" value={user?.uid} />
--- pages/api/addPhoto.ts
+++ pages/api/addPhoto.ts
@@ -5,12 +5,15 @@ import { firestore } from "@/lib/firebase";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method == "POST") {
 		const docRef = doc(firestore, "unsplash", uuidv4());
+		console.log(req.body)
 		const body = {
 			...req.body,
 			timestamp: serverTimestamp(),
 		};
+		console.log(body)
+
 		await setDoc(docRef, body);
-		res.status(201).redirect('/');
+		res.status(201).json({});
 	}
 };
 
